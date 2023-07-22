20c98e6da967b4fe869463ab430585a9a4afbf8c
Narongrit Mekpho <narongritjamenrtmp@gmail.com>
2023-07-22 18:58:22 +0700

    update orm

--- orm/fuel-price.go
+++ orm/fuel-price.go
@@ -8,10 +8,10 @@ type FuelPrice struct { //no CRUD
 	ID string `db:"id" json:"id" gorm:"type:varchar(2);primary_key;"`
 	orm.GormModel
 
-	FuelID           string  `db:"fuel_id" json:"fuel_id" gorm:"type:varchar(2);"`
-	RentalRateID     string  `db:"rental_rate_id" json:"rental_rate_id" gorm:"type:varchar(36); "`
-	RentalFuelRateID string  `db:"rental_fuel_rate_id" json:"rental_fuel_rate_id" gorm:"type:varchar(36); "`
-	Month            int     `db:"imonth" json:"imonth" gorm:"type:int(2);"`
-	Year             int     `db:"iyear" json:"iyear" gorm:"type:int(4);"`
-	Price            float64 `db:"price" json:"price" gorm:"default:null; type:decimal(16,4); comment:'ราคาค่าน้ำมันแต่ละเดือน' "`
+	FuelID    string  `db:"fuel_id" json:"fuel_id" gorm:"type:varchar(2);"`
+	StartDate string  `db:"start_date" json:"start_date" gorm:"type:varchar(50) ; comment:'วันที่เริ่ม' "`
+	EndDate   string  `db:"end_date" json:"end_date" gorm:"type:varchar(50) ; comment:'วันที่จบ'"`
+	Month     int     `db:"imonth" json:"imonth" gorm:"type:int(2);"`
+	Year      int     `db:"iyear" json:"iyear" gorm:"type:int(4);"`
+	Average   float64 `db:"average" json:"average" gorm:"default:null; type:decimal(16,4); comment:'ราคาค่าน้ำมันแต่ละเดือน' "`
 }
