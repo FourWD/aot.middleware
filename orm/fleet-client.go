c58f809d56659f779534143fa3263d9d01c57a81
Narongrit Mekpho <narongritjamenrtmp@gmail.com>
2023-07-27 11:44:08 +0700

    update orm fleet

--- orm/fleet-client.go
+++ orm/fleet-client.go
@@ -10,15 +10,17 @@ type FleetClient struct {
 
 	FleetID string `db:"fleet_id" json:"fleet_id" gorm:"type:varchar(4)"`
 
-	Name               string `db:"name" json:"name" gorm:"type:varchar(150)"`
-	RefCode            string `db:"ref_code" json:"ref_code" gorm:"type:varchar(150)"`
-	Remark             string `db:"remark" json:"remark" gorm:"type:varchar(200)"`
-	RowOrder           int    `db:"row_order" json:"row_order" gorm:"type:int"`
-	Address            string `db:"address" json:"address" gorm:"type:varchar(150)"`
-	PhoneNo            string `db:"phone_no" json:"phone_no" gorm:"type:varchar(10)"` //
-	Email              string `db:"email" json:"email" gorm:"type:varchar(50)"`
-	Status             bool   `db:"status" json:"status" gorm:"type:bool"`
-	ContactPerson      string `db:"contact_person" json:"contact_person" gorm:"type:varchar(50)"`
-	ContactPersonNo    string `db:"contact_person_no" json:"contact_person_no" gorm:"type:varchar(10)"`
-	ContactPersonEmail string `db:"contact_person_email" json:"contact_person_email" gorm:"type:varchar(50)"`
+	Name               string  `db:"name" json:"name" gorm:"type:varchar(150)"`
+	RefCode            string  `db:"ref_code" json:"ref_code" gorm:"type:varchar(150)"`
+	Remark             string  `db:"remark" json:"remark" gorm:"type:varchar(200)"`
+	RowOrder           int     `db:"row_order" json:"row_order" gorm:"type:int"`
+	Address            string  `db:"address" json:"address" gorm:"type:varchar(150)"`
+	PhoneNo            string  `db:"phone_no" json:"phone_no" gorm:"type:varchar(10)"`
+	MinTicket          int     `db:"min_ticket" json:"min_ticket" gorm:"type:int"`
+	Discount           float64 `db:"discount" json:"discount" gorm:"default:null; type:decimal(16,4); comment:'ส่วนลด' "`
+	Email              string  `db:"email" json:"email" gorm:"type:varchar(50)"`
+	Status             bool    `db:"status" json:"status" gorm:"type:bool"`
+	ContactPerson      string  `db:"contact_person" json:"contact_person" gorm:"type:varchar(50)"`
+	ContactPersonNo    string  `db:"contact_person_no" json:"contact_person_no" gorm:"type:varchar(10)"`
+	ContactPersonEmail string  `db:"contact_person_email" json:"contact_person_email" gorm:"type:varchar(50)"`
 }
