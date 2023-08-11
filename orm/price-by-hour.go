fd125832548fcfeabd821da9fea821e0dcfb513f
jamenrtmp <narongritjamenrtmp@gmail.com>
2023-08-11 11:26:10 +0700

    update orm

--- orm/price-by-hour.go
+++ orm/price-by-hour.go
@@ -8,15 +8,15 @@ type PriceByHour struct { // poi ของ รถแต่ละประเภ�
 	ID string `db:"id" json:"id" gorm:"type:varchar(36);primary_key;"`
 	orm.GormModel
 
-	Hour                   int     `db:"ihour" json:"ihour" gorm:"type:int"`
-	PriceByVehicleTypeID01 float64 `db:"price_by_vehicle_type_id_01" json:"price_by_vehicle_type_id_01" gorm:"default:null; type:decimal(10,4); column:price_by_vehicle_type_id_01; comment:'BMW' "`
-	PriceByVehicleTypeID02 float64 `db:"price_by_vehicle_type_id_02" json:"price_by_vehicle_type_id_02" gorm:"default:null; type:decimal(10,4); column:price_by_vehicle_type_id_02; comment:'BENZ' "`
-	PriceByVehicleTypeID03 float64 `db:"price_by_vehicle_type_id_03" json:"price_by_vehicle_type_id_03" gorm:"default:null; type:decimal(10,4); column:price_by_vehicle_type_id_03; comment:'Toyota Camry' "`
-	PriceByVehicleTypeID04 float64 `db:"price_by_vehicle_type_id_04" json:"price_by_vehicle_type_id_04" gorm:"default:null; type:decimal(10,4); column:price_by_vehicle_type_id_04; comment:'Toyota Commuter' "`
-	PriceByVehicleTypeID05 float64 `db:"price_by_vehicle_type_id_05" json:"price_by_vehicle_type_id_05" gorm:"default:null; type:decimal(10,4); column:price_by_vehicle_type_id_05; comment:'Isuzu Mu-x' "`
-	PriceByVehicleTypeID06 float64 `db:"price_by_vehicle_type_id_06" json:"price_by_vehicle_type_id_06" gorm:"default:null; type:decimal(10,4); column:price_by_vehicle_type_id_06; comment:'' "`
-	PriceByVehicleTypeID07 float64 `db:"price_by_vehicle_type_id_07" json:"price_by_vehicle_type_id_07" gorm:"default:null; type:decimal(10,4); column:price_by_vehicle_type_id_07; comment:'' "`
-	PriceByVehicleTypeID08 float64 `db:"price_by_vehicle_type_id_08" json:"price_by_vehicle_type_id_08" gorm:"default:null; type:decimal(10,4); column:price_by_vehicle_type_id_08; comment:'' "`
-	PriceByVehicleTypeID09 float64 `db:"price_by_vehicle_type_id_09" json:"price_by_vehicle_type_id_09" gorm:"default:null; type:decimal(10,4); column:price_by_vehicle_type_id_09; comment:'' "`
-	PriceByVehicleTypeID10 float64 `db:"price_by_vehicle_type_id_10" json:"price_by_vehicle_type_id_10" gorm:"default:null; type:decimal(10,4); column:price_by_vehicle_type_id_10; comment:'' "`
+	Hour              int     `db:"ihour" json:"ihour" gorm:"type:int"`
+	ByVehicleTypeID01 float64 `db:"by_vehicle_type_id_01" json:"by_vehicle_type_id_01" gorm:"default:null; column:by_vehicle_type_id_01; type:decimal(10,4); comment:'BMW' "`
+	ByVehicleTypeID02 float64 `db:"by_vehicle_type_id_02" json:"by_vehicle_type_id_02" gorm:"default:null; column:by_vehicle_type_id_02; type:decimal(10,4); comment:'BENZ' "`
+	ByVehicleTypeID03 float64 `db:"by_vehicle_type_id_03" json:"by_vehicle_type_id_03" gorm:"default:null; column:by_vehicle_type_id_03; type:decimal(10,4); comment:'Toyota Camry' "`
+	ByVehicleTypeID04 float64 `db:"by_vehicle_type_id_04" json:"by_vehicle_type_id_04" gorm:"default:null; column:by_vehicle_type_id_04;  type:decimal(10,4); comment:'Toyota Commuter' "`
+	ByVehicleTypeID05 float64 `db:"by_vehicle_type_id_05" json:"by_vehicle_type_id_05" gorm:"default:null; column:by_vehicle_type_id_05;  type:decimal(10,4); comment:'Isuzu Mu-x' "`
+	ByVehicleTypeID06 float64 `db:"by_vehicle_type_id_06" json:"by_vehicle_type_id_06" gorm:"default:null; column:by_vehicle_type_id_06;  type:decimal(10,4); comment:'' "`
+	ByVehicleTypeID07 float64 `db:"by_vehicle_type_id_07" json:"by_vehicle_type_id_07" gorm:"default:null; column:by_vehicle_type_id_07;  type:decimal(10,4); comment:'' "`
+	ByVehicleTypeID08 float64 `db:"by_vehicle_type_id_08" json:"by_vehicle_type_id_08" gorm:"default:null; column:by_vehicle_type_id_08;  type:decimal(10,4); comment:'' "`
+	ByVehicleTypeID09 float64 `db:"by_vehicle_type_id_09" json:"by_vehicle_type_id_09" gorm:"default:null; column:by_vehicle_type_id_09;  type:decimal(10,4); comment:'' "`
+	ByVehicleTypeID10 float64 `db:"by_vehicle_type_id_10" json:"by_vehicle_type_id_10" gorm:"default:null; column:by_vehicle_type_id_10;  type:decimal(10,4); comment:'' "`
 }
--- orm/rental-fuel-rate-by-day.go
+++ orm/rental-fuel-rate-by-day.go
@@ -7,15 +7,15 @@ import (
 type RentalFuelRateByDay struct { // ค่าเช่าค่าน้ำมันตามระยะทาง
 	ID string `db:"id" json:"id" gorm:"type:varchar(36);primary_key;"`
 	orm.GormModel
-	Day                     int     `db:"iday" json:"iday" gorm:"type:int"`
-	RentalByVehicleTypeID01 float64 `db:"rental_by_vehicle_type_id_01" json:"rental_by_vehicle_type_id_01" gorm:"default:null; column:rental_by_vehicle_type_id_01; type:decimal(10,4); comment:'BMW' "`
-	RentalByVehicleTypeID02 float64 `db:"rental_by_vehicle_type_id_02" json:"rental_by_vehicle_type_id_02" gorm:"default:null; column:rental_by_vehicle_type_id_02; type:decimal(10,4); comment:'BENZ' "`
-	RentalByVehicleTypeID03 float64 `db:"rental_by_vehicle_type_id_03" json:"rental_by_vehicle_type_id_03" gorm:"default:null; column:rental_by_vehicle_type_id_03; type:decimal(10,4); comment:'Toyota Camry' "`
-	RentalByVehicleTypeID04 float64 `db:"rental_by_vehicle_type_id_04" json:"rental_by_vehicle_type_id_04" gorm:"default:null; column:rental_by_vehicle_type_id_04;  type:decimal(10,4); comment:'Toyota Commuter' "`
-	RentalByVehicleTypeID05 float64 `db:"rental_by_vehicle_type_id_05" json:"rental_by_vehicle_type_id_05" gorm:"default:null; column:rental_by_vehicle_type_id_05;  type:decimal(10,4); comment:'Isuzu Mu-x' "`
-	RentalByVehicleTypeID06 float64 `db:"rental_by_vehicle_type_id_06" json:"rental_by_vehicle_type_id_06" gorm:"default:null; column:rental_by_vehicle_type_id_06;  type:decimal(10,4); comment:'' "`
-	RentalByVehicleTypeID07 float64 `db:"rental_by_vehicle_type_id_07" json:"rental_by_vehicle_type_id_07" gorm:"default:null; column:rental_by_vehicle_type_id_07;  type:decimal(10,4); comment:'' "`
-	RentalByVehicleTypeID08 float64 `db:"rental_by_vehicle_type_id_08" json:"rental_by_vehicle_type_id_08" gorm:"default:null; column:rental_by_vehicle_type_id_08;  type:decimal(10,4); comment:'' "`
-	RentalByVehicleTypeID09 float64 `db:"rental_by_vehicle_type_id_09" json:"rental_by_vehicle_type_id_09" gorm:"default:null; column:rental_by_vehicle_type_id_09;  type:decimal(10,4); comment:'' "`
-	RentalByVehicleTypeID10 float64 `db:"rental_by_vehicle_type_id_10" json:"rental_by_vehicle_type_id_10" gorm:"default:null; column:rental_by_vehicle_type_id_10;  type:decimal(10,4); comment:'' "`
+	Day               int     `db:"iday" json:"iday" gorm:"type:int"`
+	ByVehicleTypeID01 float64 `db:"by_vehicle_type_id_01" json:"by_vehicle_type_id_01" gorm:"default:null; column:by_vehicle_type_id_01; type:decimal(10,4); comment:'BMW' "`
+	ByVehicleTypeID02 float64 `db:"by_vehicle_type_id_02" json:"by_vehicle_type_id_02" gorm:"default:null; column:by_vehicle_type_id_02; type:decimal(10,4); comment:'BENZ' "`
+	ByVehicleTypeID03 float64 `db:"by_vehicle_type_id_03" json:"by_vehicle_type_id_03" gorm:"default:null; column:by_vehicle_type_id_03; type:decimal(10,4); comment:'Toyota Camry' "`
+	ByVehicleTypeID04 float64 `db:"by_vehicle_type_id_04" json:"by_vehicle_type_id_04" gorm:"default:null; column:by_vehicle_type_id_04;  type:decimal(10,4); comment:'Toyota Commuter' "`
+	ByVehicleTypeID05 float64 `db:"by_vehicle_type_id_05" json:"by_vehicle_type_id_05" gorm:"default:null; column:by_vehicle_type_id_05;  type:decimal(10,4); comment:'Isuzu Mu-x' "`
+	ByVehicleTypeID06 float64 `db:"by_vehicle_type_id_06" json:"by_vehicle_type_id_06" gorm:"default:null; column:by_vehicle_type_id_06;  type:decimal(10,4); comment:'' "`
+	ByVehicleTypeID07 float64 `db:"by_vehicle_type_id_07" json:"by_vehicle_type_id_07" gorm:"default:null; column:by_vehicle_type_id_07;  type:decimal(10,4); comment:'' "`
+	ByVehicleTypeID08 float64 `db:"by_vehicle_type_id_08" json:"by_vehicle_type_id_08" gorm:"default:null; column:by_vehicle_type_id_08;  type:decimal(10,4); comment:'' "`
+	ByVehicleTypeID09 float64 `db:"by_vehicle_type_id_09" json:"by_vehicle_type_id_09" gorm:"default:null; column:by_vehicle_type_id_09;  type:decimal(10,4); comment:'' "`
+	ByVehicleTypeID10 float64 `db:"by_vehicle_type_id_10" json:"by_vehicle_type_id_10" gorm:"default:null; column:by_vehicle_type_id_10;  type:decimal(10,4); comment:'' "`
 }
