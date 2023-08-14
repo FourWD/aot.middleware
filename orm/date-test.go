ebcbde648144fd0ee2311a1b71fae92a450a88c1
jamenrtmp <narongritjamenrtmp@gmail.com>
2023-08-14 15:25:53 +0700

    time.Ttime

--- orm/date-test.go
+++ orm/date-test.go
@@ -1,6 +1,8 @@
 package orm
 
 import (
+	"time"
+
 	orm "github.com/FourWD/middleware/orm"
 )
 
@@ -8,5 +10,5 @@ type DateTest struct {
 	ID string `db:"id" json:"id" gorm:"type:varchar(36);primary_key;"`
 	orm.GormModel
 
-	DateAt string `db:"date_at" json:"date_at" gorm:"default:null;  column:date_at;  type:datetime; comment:'วันเวลาที่ยกเลิก' "`
+	DateAt time.Time `db:"date_at" json:"date_at" gorm:"default:null;  column:date_at;  type:datetime; comment:'วันเวลาที่ยกเลิก' "`
 }
