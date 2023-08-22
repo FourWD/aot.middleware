1a2bcba8a0e6935bd5c1409919844e835a65d253
jamenrtmp <narongritjamenrtmp@gmail.com>
2023-08-22 18:51:02 +0700

    update orm

--- orm/vehicle-model.go
+++ orm/vehicle-model.go
@@ -8,9 +8,10 @@ type VehicleModel struct {
 
 	VehicleBrandID string `db:"vehicle_brand_id" json:"vehicle_brand_id"`
 	VehicleTypeID  string `db:"vehicle_type_id" json:"vehicle_type_id"`
-
-	Name     string `db:"name" json:"type_name"`
-	NameEn   string `db:"name_en" json:"type_name_en"`
-	RowOrder int    `db:"row_order" json:"row_order" gorm:"type:int"`
-	Image    string `db:"image" json:"image"`
+	Name           string `db:"name" json:"type_name"`
+	NameEn         string `db:"name_en" json:"type_name_en"`
+	Image          string `db:"image" json:"image"`
+	IsShow         bool   `db:"is_show" json:"is_show"  gorm:"type:bool"`
+	SubModelGroup  string `db:"sub_model_group" json:"sub_model_group" gorm:"type:varchar(5)"`
+	RowOrder       int    `db:"row_order" json:"row_order" gorm:"type:int"`
 }
