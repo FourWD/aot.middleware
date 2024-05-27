e181992b0175f7e121db2cd0bda406a93fda09d8
puarns <puarns@gmail.com>
2024-05-27 16:14:22 +0700

    no message

--- orm/configs.go
+++ orm/configs.go
@@ -5,9 +5,9 @@ import (
 )
 
 type Configs struct {
-	ID string `db:"id" json:"id" gorm:"type:varchar(36);primary_key;"`
+	ID string `json:"id" query:"id" gorm:"type:varchar(36);primary_key;"`
 	orm.GormModel
 
-	Name        string `db:"name" json:"name" gorm:"type:varchar(150)"`
-	ConfigValue string `db:"config_value" json:"config_value" gorm:"type:varchar(150)"`
+	Name        string `json:"name" query:"name" gorm:"type:varchar(150)"`
+	ConfigValue string `json:"config_value" query:"config_value" gorm:"type:varchar(150)"`
 } // void ผ่าน callcenter 100 % // void ผ่าน website 95%
