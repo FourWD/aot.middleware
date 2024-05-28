82793ef1e7ac0ec64bf042950ab399f77e648a19
Narongrit Mekpho <narongritjamenrtmp@gmail.com>
2024-05-28 14:14:54 +0700

    update slips

--- orm/slip.go
+++ orm/slip.go
@@ -1,13 +1,6 @@
 package orm
 
-import (
-	orm "github.com/FourWD/middleware/model"
-)
-
 type Slip struct {
 	SlipModel
-
-	ID string `json:"id" query:"id" gorm:"type:varchar(36);primary_key;"`
-	orm.GormModel
 	JobID string `json:"job_id" query:"job_id" gorm:"type:varchar(10);comment:'หมายเลขงาน'"` //หมายเลขงาน
 }
