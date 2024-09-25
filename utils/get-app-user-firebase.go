30bd28f93340721fbb9ca7b34a2e3d707bf4eb83
PAKKAD\puarns <puarns@gmail.com>
2024-09-25 15:42:48 +0700

    no message

---
+++ utils/get-app-user-firebase.go
@@ -0,0 +1,26 @@
+package utils
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/FourWD/aot.middleware/orm"
+	"github.com/FourWD/middleware/common"
+)
+
+func GetAppUserFirebase(userID string) (orm.AppUser, error) {
+	docPath := fmt.Sprintf("users/%s", userID)
+	docRef := common.FirebaseClient.Doc(docPath)
+	snap, err := docRef.Get(context.Background())
+	if err != nil {
+		return orm.AppUser{}, errors.New("cannot get : " + docPath)
+	}
+
+	var appUser orm.AppUser
+	if err := snap.DataTo(&appUser); err != nil {
+		return orm.AppUser{}, errors.New("cannot snap : " + docPath)
+	}
+
+	return appUser, nil
+}
