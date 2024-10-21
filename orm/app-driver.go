ad49cd684c116140bb1c378224abaa9828d37507
Narongrit Mekpho <narongritjamenrtmp@gmail.com>
2024-10-21 16:04:22 +0700

    Update app-driver.go

--- orm/app-driver.go
+++ orm/app-driver.go
@@ -3,10 +3,10 @@ package orm
 type AppDriver struct {
 	Slip
 	// AppDriverStatusID string `json:"app_driver_status_id" query:"app_driver_status_id" firestore:"app_driver_status_id" gorm:"type:varchar(2);"`
-	VehicleModelID string `json:"vehicle_model_id"  firestore:"vehicle_model_id" gorm:"-"`
-	SlipID         string `json:"slip_id" firestore:"slip_id" query:"slip_id" gorm:"-"`
-	VehicleName    string `json:"vehicle_name" firestore:"vehicle_name" query:"vehicle_name" gorm:"-"`
-	// DriverImagePath     string `json:"driver_image_path"  firestore:"driver_image_path"`
+	VehicleModelID  string `json:"vehicle_model_id"  firestore:"vehicle_model_id" gorm:"-"`
+	SlipID          string `json:"slip_id" firestore:"slip_id" query:"slip_id" gorm:"-"`
+	VehicleName     string `json:"vehicle_name" firestore:"vehicle_name" query:"vehicle_name" gorm:"-"`
+	IsOriginAirport string `json:"is_origin_airport" firestore:"is_origin_airport" query:"is_origin_airport" gorm:"-"`
 	// Name                string `json:"name"  firestore:"name" `
 	// VehicleID           string `json:"vehicle_id"  firestore:"driver_register_id" `
 	// VehicleCode         string `json:"vehicle_code"  firestore:"vehicle_id"`
