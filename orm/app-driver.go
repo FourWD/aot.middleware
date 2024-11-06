bf481cc779a7fdd6d1cb0c92e18053a49889d6f7
PAKKAD\puarns <puarns@gmail.com>
2024-11-06 14:07:08 +0700

    no message

--- orm/app-driver.go
+++ orm/app-driver.go
@@ -2,6 +2,14 @@ package orm
 
 type AppDriver struct {
 	Slip
+	LoginDriverName         string `json:"login_driver_name" firestore:"login_driver_name" query:"login_driver_name" gorm:"-"`
+	LoginVehicleTypeID      string `json:"login_vehicle_type_id" firestore:"login_vehicle_type_id" query:"login_vehicle_type_id" gorm:"-"`
+	LoginVehicleTypeName    string `json:"login_vehicle_type_name" firestore:"login_vehicle_type_name" query:"login_vehicle_type_name" gorm:"-"`
+	LoginVehicleSubTypeID   string `json:"login_vehicle_sub_type_id" firestore:"login_vehicle_sub_type_id" query:"login_vehicle_sub_type_id" gorm:"-"`
+	LoginVehicleSubTypeName string `json:"login_vehicle_sub_type_name" firestore:"login_vehicle_sub_type_name" query:"login_vehicle_sub_type_name" gorm:"-"`
+	LoginVehicleModelID     string `json:"login_vehicle_model_id" firestore:"login_vehicle_model_id" query:"login_vehicle_model_id" gorm:"-"`
+	LoginVehicleModelName   string `json:"login_vehicle_model_name" firestore:"login_vehicle_model_name" query:"login_vehicle_model_name" gorm:"-"`
+
 	SlipID             string `json:"slip_id" firestore:"slip_id" query:"slip_id" gorm:"-"`
 	VehicleSubTypeID   string `json:"vehicle_sub_type_id" firestore:"vehicle_sub_type_id" query:"vehicle_sub_type_id" gorm:"-"`
 	VehicleSubTypeName string `json:"vehicle_sub_type_name" firestore:"vehicle_sub_type_name" query:"vehicle_sub_type_name" gorm:"-"`
