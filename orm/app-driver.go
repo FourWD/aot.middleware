d271a177d55304ba476068a9a944361f83007ef9
PAKKAD\puarns <puarns@gmail.com>
2024-11-07 13:22:55 +0700

    no message

--- orm/app-driver.go
+++ orm/app-driver.go
@@ -10,16 +10,18 @@ type AppDriver struct {
 	LoginVehicleModelID     string `json:"login_vehicle_model_id" firestore:"login_vehicle_model_id" query:"login_vehicle_model_id" gorm:"-"`
 	LoginVehicleModelName   string `json:"login_vehicle_model_name" firestore:"login_vehicle_model_name" query:"login_vehicle_model_name" gorm:"-"`
 
-	SlipID             string `json:"slip_id" firestore:"slip_id" query:"slip_id" gorm:"-"`
+	SlipID          string `json:"slip_id" firestore:"slip_id" query:"slip_id" gorm:"-"`
+	IsOriginAirport bool   `json:"is_origin_airport" firestore:"is_origin_airport" query:"is_origin_airport" gorm:"-"`
+
+	LicensePlate       string `json:"license_plate" firestore:"license_plate" query:"license_plate" gorm:"-"`
+	VehicleCode        string `json:"vehicle_code" firestore:"vehicle_code" query:"vehicle_code" gorm:"-"`
+	VehicleImage       string `json:"vehicle_image" firestore:"vehicle_image" query:"vehicle_image" gorm:"-"`
+	VehicleColorName   string `json:"vehicle_color_name" firestore:"vehicle_color_name" query:"vehicle_color_name" gorm:"-"`
 	VehicleSubTypeID   string `json:"vehicle_sub_type_id" firestore:"vehicle_sub_type_id" query:"vehicle_sub_type_id" gorm:"-"`
 	VehicleSubTypeName string `json:"vehicle_sub_type_name" firestore:"vehicle_sub_type_name" query:"vehicle_sub_type_name" gorm:"-"`
 	VehicleModelID     string `json:"vehicle_model_id" firestore:"vehicle_model_id" query:"vehicle_model_id" gorm:"-"`
 	VehicleModelName   string `json:"vehicle_model_name" firestore:"vehicle_model_name" query:"vehicle_model_name" gorm:"-"`
-	IsOriginAirport    bool   `json:"is_origin_airport" firestore:"is_origin_airport" query:"is_origin_airport" gorm:"-"`
-	DriverName         string `json:"driver_name" firestore:"driver_name" query:"driver_name" gorm:"-"`
-	VehicleCode        string `json:"vehicle_code" firestore:"vehicle_code" query:"vehicle_code" gorm:"-"`
-	DriverImage        string `json:"driver_image" firestore:"driver_image" query:"driver_image" gorm:"-"`
-	VehicleImage       string `json:"vehicle_image" firestore:"vehicle_image" query:"vehicle_image" gorm:"-"`
-	VehicleColorName   string `json:"vehicle_color_name" firestore:"vehicle_color_name" query:"vehicle_color_name" gorm:"-"`
-	LicensePlate       string `json:"license_plate" firestore:"license_plate" query:"license_plate" gorm:"-"`
+
+	DriverName  string `json:"driver_name" firestore:"driver_name" query:"driver_name" gorm:"-"`
+	DriverImage string `json:"driver_image" firestore:"driver_image" query:"driver_image" gorm:"-"`
 }
