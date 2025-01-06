4ea946627701b5819534d9a47c3b04416fd0bb2b
Plew Woo <pew_woothiphom@hotmail.com>
2025-01-06 17:13:22 +0700

    Update slip.go

--- orm/slip.go
+++ orm/slip.go
@@ -96,11 +96,11 @@ type Slip struct {
 	// RentalPrice       float64   `json:"rental_price" query:"rental_price" firestore:"rental_price" gorm:"type:decimal(16,4)"`
 	// RentalFuelRateID  string    `json:"rental_fuel_rate_id" query:"rental_fuel_rate_id" firestore:"rental_fuel_rate_id" gorm:"type:varchar(20)"`
 	// RentalFuelLitre   float64   `json:"rental_fuel_litre" query:"rental_fuel_litre" firestore:"rental_fuel_litre" gorm:"type:decimal(16,4)"`
-	FuelAverage       float64 `json:"fuel_average" query:"fuel_average" firestore:"fuel_average" gorm:"type:decimal(16,4);comment:'เก็บราคาของค่าน้ำมันเฉลี่ย '"`
-	AssignVehicleID   string  `json:"assign_vehicle_id" query:"assign_vehicle_id" firestore:"assign_vehicle_id" gorm:"type:varchar(36);comment:'รถที่วิ่งงานจริง'"`
-	AssignVehicleBy   string  `json:"assign_vehicle_by" query:"assign_vehicle_by" firestore:"assign_vehicle_by" gorm:"type:varchar(36);"`
-	AssignDriverID    string  `json:"assign_driver_id" query:"assign_driver_id" firestore:"assign_driver_id" gorm:"default:null; type:varchar(36); "`
-	JobAssignStatusID string  `json:"job_assign_status_id" query:"job_assign_status_id" firestore:"job_assign_status_id" gorm:"default:'00'; type:varchar(36)"`
+	FuelAverage     float64 `json:"fuel_average" query:"fuel_average" firestore:"fuel_average" gorm:"type:decimal(16,4);comment:'เก็บราคาของค่าน้ำมันเฉลี่ย '"`
+	AssignVehicleID string  `json:"assign_vehicle_id" query:"assign_vehicle_id" firestore:"assign_vehicle_id" gorm:"type:varchar(36);comment:'รถที่วิ่งงานจริง'"`
+	AssignVehicleBy string  `json:"assign_vehicle_by" query:"assign_vehicle_by" firestore:"assign_vehicle_by" gorm:"type:varchar(36);"`
+	AssignDriverID  string  `json:"assign_driver_id" query:"assign_driver_id" firestore:"assign_driver_id" gorm:"default:null; type:varchar(36); "`
+	// JobAssignStatusID string  `json:"job_assign_status_id" query:"job_assign_status_id" firestore:"job_assign_status_id" gorm:"default:'00'; type:varchar(36)"`
 
 	IsArrive        bool      `json:"is_arrive" query:"is_arrive" firestore:"is_arrive" gorm:"default:null;"`
 	ArriveDate      time.Time `json:"arrive_date" query:"arrive_date" firestore:"arrive_date" gorm:"default:null;comment:'วันเวลาที่มาถึง'"`
