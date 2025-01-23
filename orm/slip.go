90f2ae41cda9d439f7181d897a3bd35ec1b21a0d
Plew Woo <pew_woothiphom@hotmail.com>
2025-01-23 11:10:55 +0700

    Update SlipTemp

--- orm/slip.go
+++ orm/slip.go
@@ -3,4 +3,5 @@ package orm
 type Slip struct {
 	ID string `json:"id" query:"id" firestore:"ID" gorm:"type:varchar(36);unique;"`
 	SlipTemp
+	RunningNo int `json:"running_no" query:"running_no" firestore:"running_no" gorm:"primary_key;auto_increment;not_null"`
 }
