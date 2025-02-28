5174375629bcf9c38706225d1092b9dfa6058066
Plew Woo <pew_woothiphom@hotmail.com>
2025-02-28 10:18:00 +0700

    Update JobNo

--- orm/slip.go
+++ orm/slip.go
@@ -3,5 +3,6 @@ package orm
 type Slip struct {
 	ID string `json:"id" query:"id" firestore:"ID" gorm:"type:varchar(36);unique;"`
 	SlipTemp
-	RunningNo int `json:"running_no" query:"running_no" firestore:"running_no" gorm:"primary_key;auto_increment;not_null"`
+	JobNo     string `json:"job_no" query:"job_no" firestore:"job_no" gorm:"type:varchar(36);unique"`
+	RunningNo int    `json:"running_no" query:"running_no" firestore:"running_no" gorm:"primary_key;auto_increment;not_null"`
 }
