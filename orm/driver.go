b8ab1e4706deb924bbc2db209eece9f0f318a844
Plew Woo <pew_woothiphom@hotmail.com>
2025-03-13 17:57:33 +0700

    Update driver.go

--- orm/driver.go
+++ orm/driver.go
@@ -14,28 +14,28 @@ type Driver struct {
 	Firstname               string    `json:"firstname" query:"firstname" gorm:"type:varchar(100)"`
 	Lastname                string    `json:"lastname" query:"lastname" gorm:"type:varchar(100)"`
 	Nickname                string    `json:"nickname" query:"nickname" gorm:"type:varchar(50)"`
-	Birthday                time.Time `json:"birthday" query:"birthday" `
+	Birthday                time.Time `json:"birthday" query:"birthday" gorm:"default:null;"`
 	Email                   string    `json:"email" query:"email" gorm:"type:varchar(100)"`
 	IDCardNo                string    `json:"idcard_no" query:"idcard_no" gorm:"type:varchar(13)"`
 	DriverLicenseNo         string    `json:"driver_license_no" query:"driver_license_no" gorm:"type:varchar(13)"`
-	DriverLicenseExpireDate time.Time `json:"driver_license_expire_date" query:"driver_license_expire_date" gorm:"type:varchar(50)"`
+	DriverLicenseExpireDate time.Time `json:"driver_license_expire_date" query:"driver_license_expire_date" gorm:"default:null;"`
 	PhoneNo                 string    `json:"phone_no" query:"phone_no" gorm:"type:varchar(50)"`
 	Address                 string    `json:"address" query:"address" gorm:"type:varchar(150)"`
-	WorkingStartDate        time.Time `json:"working_start_date" query:"working_start_date"`
-	WorkingEndDate          time.Time `json:"working_end_date" query:"working_end_date"`
+	WorkingStartDate        time.Time `json:"working_start_date" query:"working_start_date" gorm:"default:null;"`
+	WorkingEndDate          time.Time `json:"working_end_date" query:"working_end_date" gorm:"default:null;"`
 	Remark                  string    `json:"remark" query:"remark" gorm:"type:varchar(200)"`
 	DriverImagePath         string    `json:"driver_image_path" query:"driver_image_path" gorm:"type:varchar(200)"`
 	DefaultVehicleModelID   string    `json:"default_vehicle_model_id" query:"default_vehicle_model_id" gorm:"default:null;type:varchar(2);"`
-	CurrentVehicleID        string    `json:"current_vehicle_id" query:"current_vehicle_id" gorm:"default:null;type:varchar(36);"`
+	CurrentVehicleID        string    `json:"current_vehicle_id" query:"current_vehicle_id" gorm:"default:null; type:varchar(36);"`
 	SlipID                  string    `json:"slip_id" query:"slip_id" gorm:"type:varchar(36); comment:'รับงานอะไรอยู่'"`
 	GenderID                string    `json:"gender_id" query:"gender_id" gorm:"type:varchar(2); comment:'รับงานอะไรอยู่'"`
 	IsOnline                bool      `json:"is_online" query:"is_online" gorm:"type:bool"`
 	IsActive                bool      `json:"is_active" query:"is_active" gorm:"type:bool"`
 	IsAppActive             bool      `json:"is_app_active" query:"is_app_active" gorm:"type:bool"`
-	LastOnlineDate          time.Time `json:"last_online_date" query:"last_online_date"`
+	LastOnlineDate          time.Time `json:"last_online_date" query:"last_online_date" gorm:"default:null;"`
 	QueueNo                 string    `json:"queue_no" query:"queue_no" gorm:"type:varchar(10)"` // stamp from running_nos
-	QueueDate               time.Time `json:"queue_date" query:"queue_date"`
-	TerminalQueueDate       time.Time `json:"terminal_queue_date" query:"terminal_queue_date" gorm:"default:null"`
+	QueueDate               time.Time `json:"queue_date" query:"queue_date" gorm:"default:null;"`
+	TerminalQueueDate       time.Time `json:"terminal_queue_date" query:"terminal_queue_date" gorm:"default:null;"`
 	GateID                  string    `json:"gate_id" query:"gate_id" gorm:"type:varchar(2)"`
 	StopLocationID          int       `json:"stop_location_id" query:"stop_location_id" gorm:"default:null; type:int;"`
 }
