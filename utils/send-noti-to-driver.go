1efa07eb16b6a62761d86bed16f1ff99c9fb2964
PAKKAD\puarns <puarns@gmail.com>
2025-03-30 16:13:16 +0700

    no message

--- utils/send-noti-to-driver.go
+++ utils/send-noti-to-driver.go
@@ -1,13 +1,17 @@
 package utils
 
 import (
+	"fmt"
+
 	"github.com/FourWD/middleware/common"
 )
 
 func SendNotiToDriver(driverID, slipID, typeID string) {
 	title := "NOTI-DRIVER"
 	body := "TEST DRIVER"
+	topic := fmt.Sprintf("DRIVER_%s", driverID)
 	logData := map[string]interface{}{
+		"topic":    topic,
 		"driverID": driverID,
 		"slipID":   slipID,
 		"typeID":   typeID,
@@ -21,5 +25,5 @@ func SendNotiToDriver(driverID, slipID, typeID string) {
 		// "event_code": "NT001",
 	}
 
-	common.SendMessageToSubscriber(driverID, title, body, data)
+	common.SendMessageToSubscriber(topic, title, body, data)
 }
