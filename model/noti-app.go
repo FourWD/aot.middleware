f775f0f0f07403dcd4885a06c963d8134c9a4a99
PAKKAD\puarns <puarns@gmail.com>
2025-06-18 01:59:42 +0700

    no message

--- model/noti-app.go
+++ model/noti-app.go
@@ -5,7 +5,7 @@ import "time"
 type NotiApp struct {
 	SlipID     string    `json:"slip_id" query:"slip_id"`
 	VehicleID  string    `json:"vehicle_id" query:"vehicle_id"`
-	DriverID   int       `json:"driver_id" query:"driver_id"`
+	DriverID   string    `json:"driver_id" query:"driver_id"`
 	UserID     string    `json:"user_id" query:"user_id"`
 	ActionTime time.Time `json:"action_time" query:"action_time"`
 }
