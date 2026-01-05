b210dc1393154e74baffe87b5d2a098cebed7693
Plew Woo <pew_woothiphom@hotmail.com>
2026-01-05 17:24:16 +0700

    Update slip-temp.go

--- orm/slip-temp.go
+++ orm/slip-temp.go
@@ -116,7 +116,7 @@ type SlipTemp struct {
 	BookingDate time.Time `json:"booking_date" query:"booking_date" firestore:"booking_date" gorm:"default:null;"`
 
 	//---FLEET--
-	FleetClientID        string `json:"fleet_client_id" query:"fleet_client_id" firestore:"fleet_client_id" gorm:"type:varchar(10); default:null;"`
+	FleetClientID        string `json:"fleet_client_id" query:"fleet_client_id" firestore:"fleet_client_id" gorm:"type:varchar(20); default:null;"`
 	BookingHour          string `json:"booking_hour" query:"booking_hour" firestore:"booking_hour" gorm:"type:varchar(2);"`
 	Passenger            int    `json:"passenger" query:"passenger" firestore:"passenger" gorm:"type:int"`
 	NumberOfLuggage      int    `json:"number_of_luggage" query:"number_of_luggage" firestore:"number_of_luggage" gorm:"type:int"`
