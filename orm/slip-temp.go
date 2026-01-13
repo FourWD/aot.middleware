86b921f0021f520a6894a0cbbd811527c2d21749
Narongrit Mekpho <narongritjamenrtmp@gmail.com>
2026-01-13 10:16:47 +0700

    Update slip-temp.go

--- orm/slip-temp.go
+++ orm/slip-temp.go
@@ -266,5 +266,7 @@ type SlipTemp struct {
 	ConfirmCallDate time.Time `json:"confirm_call_date" query:"confirm_call_date" firestore:"confirm_call_date" gorm:"default:null;"`
 
 	VipID string `json:"vip_id" query:"vip_id" firestore:"vip_id" gorm:"type:varchar(36);"`
+
+	Language string `json:"language" query:"language" firestore:"language" gorm:"type:varchar(5); default:'TH';"`
 	// ========================================================================================
 }
